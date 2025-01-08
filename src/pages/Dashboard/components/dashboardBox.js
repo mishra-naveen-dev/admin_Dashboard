a580ba54aecc9c048d3db4ee0327b65b801654f2
nvineng <naveen211063cse@sdbc.ac.in>
2025-01-08 17:27:47 +0530

    dashboard features are going on

--- src/pages/Dashboard/components/dashboardBox.js
+++ src/pages/Dashboard/components/dashboardBox.js
@@ -1,19 +1,39 @@
 // Desc: Dashboard Box component
+import React from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { LuTrendingUp, LuTrendingDown } from "react-icons/lu";
-
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
 
+
+
 const DashboardBox = (props) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const ITEM_HEIGHT = 48;
+
+  const backgroundStyle = Array.isArray(props.color)
+    ? `linear-gradient(to right,${props.color[0]},${props.color[1]})`
+    : "none";
+
   return (
     <Button
       className="dashboardBox"
       style={{
-        backgroundImage: `linear-gradient(to right,${props.color?.[0]},${props.color?.[1]})`,
+        backgroundImage: backgroundStyle,
       }}
     >
       {props.grow === true ? (
-        <span className="chart ">
+        <span className="chart">
           <LuTrendingUp />
         </span>
       ) : (
@@ -24,25 +44,51 @@ const DashboardBox = (props) => {
 
       <div className="d-flex w-100">
         <div className="col1">
-          <h4 className="text-white mb-0">Total Users</h4>
-          <span className="text-white">277</span>
+          <h4 className="text-white mb-0">{props.title}</h4>
+          <span className="text-white">{props.value}</span>
         </div>
         <div className="ml-auto">
-          {props.icon ? (
-            <span className="icon">{props.icon ? props.icon : ""}</span>
-          ) : (
-            ""
-          )}
+          {props.icon ? <span className="icon">{props.icon}</span> : ""}
         </div>
       </div>
 
       <div className="d-flex align-items-center w-100 bottomEle">
         <h6 className="text-white mb-0 mt-0">Last Month</h6>
-        <Button className="ml-auto toggleIcon">
-          <BsThreeDotsVertical />
-        </Button>
+        <div className="ml-auto">
+          <Button className="ml-auto toggleIcon" onClick={handleClick}>
+            <BsThreeDotsVertical />
+          </Button>
+          <Menu
+          className="dropDown_menu"
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            slotProps={{
+              paper: {
+                style: {
+                  maxHeight: ITEM_HEIGHT * 4.5,
+                  width: "20ch",
+                },
+              },
+            }}
+          >
+            <MenuItem key="lastDay" selected={false} onClick={handleClose}>
+              Last Day
+            </MenuItem>
+            <MenuItem key="lastWeek" selected={false} onClick={handleClose}>
+              Last Week
+            </MenuItem>
+            <MenuItem key="lastMonth" selected={false} onClick={handleClose}>
+              Last Month
+            </MenuItem>
+          </Menu>
+        </div>
       </div>
     </Button>
   );
 };
+
 export default DashboardBox;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,8 +3,26 @@ import { FaUserCircle } from "react-icons/fa";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 import { IoBagCheck } from "react-icons/io5";
 import { GrStar } from "react-icons/gr";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import Button from "@mui/material/Button";
+
+import React from "react";
 import DashboardBox from "./components/dashboardBox";
 const Dashboard = () => {
+  const ITEM_HEIGHT = 48;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       {/* <h4>Dashboard Page</h4> */}
@@ -13,19 +31,27 @@ const Dashboard = () => {
           <div className="col-md-8">
             <div className="dashboardBoxWrapper d-flex">
               <DashboardBox
+                title="Total Users"
+                value="277"
                 color={["#1da256", "#48d483"]}
                 icon={<FaUserCircle />}
                 grow={true}
               />
               <DashboardBox
+                title="Total Orders"
+                value="150"
                 color={["#c012e2", "#eb64fe"]}
                 icon={<HiOutlineShoppingCart />}
               />
               <DashboardBox
+                title="Total Orders"
+                value="120"
                 color={["#2c78e5", "#60aff5"]}
                 icon={<IoBagCheck />}
               />
               <DashboardBox
+                title="Total Reviews"
+                value="85"
                 color={["#e1950e", "#f3cd29"]}
                 icon={<GrStar />}
                 grow={true}
@@ -34,7 +60,55 @@ const Dashboard = () => {
           </div>
 
           <div className="col-md-4 pl-0">
-            <div className="box"></div>
+            <div className="box graphBox">
+              <div className="d-flex align-items-center w-100 bottomEle">
+                <h4 className="text-white mb-0 mt-0">Total Sales</h4>
+                <div className="ml-auto">
+                  <Button className="ml-auto toggleIcon" onClick={handleClick}>
+                    <BsThreeDotsVertical />
+                  </Button>
+                  <Menu
+                    className="dropDown_menu"
+                    MenuListProps={{
+                      "aria-labelledby": "long-button",
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    slotProps={{
+                      paper: {
+                        style: {
+                          maxHeight: ITEM_HEIGHT * 4.5,
+                          width: "20ch",
+                        },
+                      },
+                    }}
+                  >
+                    <MenuItem
+                      key="lastDay"
+                      selected={false}
+                      onClick={handleClose}
+                    >
+                      Last Day
+                    </MenuItem>
+                    <MenuItem
+                      key="lastWeek"
+                      selected={false}
+                      onClick={handleClose}
+                    >
+                      Last Week
+                    </MenuItem>
+                    <MenuItem
+                      key="lastMonth"
+                      selected={false}
+                      onClick={handleClose}
+                    >
+                      Last Month
+                    </MenuItem>
+                  </Menu>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
